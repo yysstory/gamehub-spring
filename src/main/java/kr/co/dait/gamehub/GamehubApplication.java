f3ec153d6a4d2868cd3b2e827808073c60108c98
yun <fullstack@kakao.com>
2023-04-20 21:51:00 +0900

    first commit

---
+++ src/main/java/kr/co/dait/gamehub/GamehubApplication.java
@@ -0,0 +1,13 @@
+package kr.co.dait.gamehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamehubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamehubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/dait/gamehub/GamehubApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.dait.gamehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamehubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
